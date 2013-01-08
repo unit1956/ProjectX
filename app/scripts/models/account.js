62a3b605b83bdecb0339eca5ff966df4601910ce
Benoit Grelard <benoit.grelard@gmail.com>
2013-01-08 10:04:01 +0000

    added basic transaction, transactions, account architecture

---
+++ app/scripts/models/account.js
@@ -0,0 +1,13 @@
+var Account = Backbone.Model.extend({
+
+	defaults: {
+		'name': 'Default account name',
+		'currency': '£',
+		'initialBalance': 0,
+		'reconciledBalance': 0,
+		'availableBalance': 0,
+		'monthEnd': null,
+		'transactions': new Transactions()
+	}
+
+});
